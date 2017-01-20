a5a388c7d13eb7c9d2a79176b3fdc4b33965f776
Harsha Rao <harsharaoesq@gmail.com>
2017-01-20 12:04:00 -0600

    My first commit

---
+++ Class project 1/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Class project 1
+//
+//  Created by Harsha Rao on 1/18/17.
+//  Copyright © 2017 Harsha Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        if coolLabel.text == "Where am I?"{
+            coolLabel.text = "Hello world!"
+        } else {
+            coolLabel.text = "Where am I?"
+        }
+        
+        tapCount += 1
+        
+        if tapCount >= 10{
+            coolLabel.text = "Bitch, stop pressing me!"
+        }
+    }
+    
+    @IBAction func secondButtonTapped(_ sender: Any) {
+        coolLabel.text = "Buttons are cool!"
+        print("button tapped")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
